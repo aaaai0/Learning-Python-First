f2bf3bc300962129f484e3e9e9647c60be31b0eb
aiiii01 <krukov.16323@gmail.com>
2024-08-13 21:12:37 +0400

    Beginning - making the code more readable

---
+++ comment.py
@@ -0,0 +1,20 @@
+# This comment, and it is invisible for IDE Python
+
+"""
+This
+comment
+too
+"""
+
+''' And
+this too '''
+
+# The comments don't mean anything for IDE
+
+print("Not comment")
+print("""
+And this not comment too
+""")
+everything = ("#Not comment")
+
+# In functions and variables ' " these symbols indicate that they contain a string; # has no meaning.
