af6430539ab579d272060ed28a598885d75f735d
aiiii01 <krukov.16323@gmail.com>
2024-08-18 17:30:23 +0400

    Update print.py

--- print.py
+++ print.py
@@ -1,19 +1,24 @@
 # The command "print" write a message to the command line
 
-print("You message text", end="")
+print("You message text \n")
 
 # If you want to output several lines, then:
 print("You")
 print("message")
-print("text", end="")
+print("text \n")
 
 # else:
-print("You \nmessage \ntext", end="")
+print("You \nmessage \ntext \n")
 
 # \n makes a line break 
 # If you want print "\" you need write "\\"
-print("You \\nmessage \\ntext", end="") #  \n will not work under these conditions
+print("You \\nmessage \\ntext \n") #  \n will not work under these conditions
 
-print("You \\message \\text", end="")
+print("You \\message \\text \n")
 
 # end="" = \n but it only works at the end of the line and you need to write print again
+
+# if you want print ' or " :
+
+print("\'")
+print("\"")
